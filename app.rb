ac68000eeb0e478ae35b6c7febf9d63a5b02824f
iwilliams83 <indirajamurath@gmail.com>
2018-06-25 20:11:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,8 +21,8 @@ class App < Sinatra::Base
 
   get "/multiply/:num1/:num2" do
     puts "params = #{params}"
-    num1 = params[:num1]
-    num2 = params[:num2]
+    puts "#{params[:num1]}"
+    #num2 = params[:num2]
     "#{num1*num2}"
   end
 
